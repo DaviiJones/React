3517fa00f7d8599fea88fd489c1d7c1d904a3257
DaviiJones <davonbridgett@pursuit.org>
2023-07-26 21:48:48 -0400

    pushed old lab into react app

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import.meta.env.VITE_KEY;
+console.log(`${import.meta.env.VITE_KEY}`)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
